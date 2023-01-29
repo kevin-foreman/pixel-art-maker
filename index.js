3fc57e91ccb3d19c035efc02c58350bcd53494e8
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-01-29 11:55:16 -0600

    Added square elements, and color will change on click

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-let gridSquare1 = document.getElementsById("grid-square-1");
-let gridSquare2 = document.getElementsById("grid-square-2");
-let gridSquare3 = document.getElementsById("grid-square-3");
-let gridSquare4 = document.getElementsById("grid-square-4");
+let gridSquare1 = document.getElementById("grid-square-1");
+let gridSquare2 = document.getElementById("grid-square-2");
+let gridSquare3 = document.getElementById("grid-square-3");
+let gridSquare4 = document.getElementById("grid-square-4");
 
 gridSquare1.addEventListener("click", (event) => {
-    console.log("Square 1 was clicked!")
+    console.log("Square 1 was clicked!");
+    gridSquare1.classList.toggle("change-to-red");
+});
+
+gridSquare2.addEventListener("click", (event) => {
+    console.log("Square 2 was clicked!");
+    gridSquare2.classList.toggle("change-to-red");
+});
+
+gridSquare3.addEventListener("click", (event) => {
+    console.log("Square 3 was clicked!");
+    gridSquare3.classList.toggle("change-to-red");
+});
+
+gridSquare4.addEventListener("click", (event) => {
+    console.log("Square 4 was clicked!");
+    gridSquare4.classList.toggle("change-to-red");
 });
 
