933d31bad792391bd5ea8c7869accccab5fef313
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-01-29 10:40:38 -0600

    Added style.css and index.js along with first event listener

---
+++ index.js
@@ -0,0 +1,9 @@
+let gridSquare1 = document.getElementsById("grid-square-1");
+let gridSquare2 = document.getElementsById("grid-square-2");
+let gridSquare3 = document.getElementsById("grid-square-3");
+let gridSquare4 = document.getElementsById("grid-square-4");
+
+gridSquare1.addEventListener("click", (event) => {
+    console.log("Square 1 was clicked!")
+});
+
